d68ba67dfbb8b724f2b3b90c9ad9449e07a3767c
Artem Nikolenko <kopo4e.web@gmail.com>
2020-12-08 10:24:02 +0300

    Fix: self method of Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,15 @@
 class Test < ApplicationRecord
   belongs_to :category
 
-  def self.find_tests_by_category(category)
-    Test.joins('JOIN categories ON tests.category_id = categories.id')
+  def self.title_tests_by_category(category)
+    title_tests = []
+
+    Test.select(:title)
+      .joins(:category)
       .where('categories.title = ?', category)
+      .order(created_at: :desc)
+      .each { |test| title_tests << test.title }
+
+    title_tests
   end
 end
