c5c041da2ad6ada81a40bbfb15bca3552835ac95
Artem Nikolenko <kopo4e.web@gmail.com>
2020-12-08 11:06:58 +0300

    Fix: optimization self method

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,14 +2,9 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.title_tests_by_category(category)
-    title_tests = []
-
-    Test.select(:title)
-      .joins(:category)
+    Test.joins(:category)
       .where('categories.title = ?', category)
       .order(created_at: :desc)
-      .each { |test| title_tests << test.title }
-
-    title_tests
+      .pluck(:title)
   end
 end
