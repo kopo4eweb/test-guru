c00d25a6bc46040ebf6468e77d97f8aa4615deb1
Artem Nikolenko <kopo4e.web@gmail.com>
2020-12-09 22:48:27 +0300

    Fix: users author before setup seeds tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,12 +5,13 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  belongs_to :user
   belongs_to :author, class_name: "User", foreign_key: "user_id"
 
   def self.title_tests_by_category(category)
     Test.joins(:category)
-      .where('categories.title = ?', category)
-      .order(created_at: :desc)
-      .pluck(:title)
+    .where('categories.title = ?', category)
+    .order(created_at: :desc)
+    .pluck(:title)
   end
 end
