e35ccb19f467e18fd06ba3840066d86814c949ab
Artem Nikolenko <kopo4e.web@gmail.com>
2020-12-09 22:49:48 +0300

    Fix: associations tests for tests_users and test owner

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  has_many :tests
-  
+  has_many :tests_owner, class_name: 'Test'
+
   def find_tests_by_level(level)
     Test.joins('JOIN passed_tests ON tests.id = passed_tests.test_id')
       .where('passed_tests.user_id = ? AND tests.level = ?', id, level)
