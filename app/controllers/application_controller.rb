0eb318d0aab29c132ec841ee14bd696ded4feda0
Artem Nikolenko <kopo4e.web@gmail.com>
2020-12-22 14:34:54 +0300

    Add: Admin panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_record_not_found
+
+  private
+
+  def rescue_record_not_found
+    render plain: "404 Not Found", status: 404
+  end
 end
